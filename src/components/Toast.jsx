69cd74b669140a2a0d3fd44cebac0a128afbddd7
Naufal Pramudya Yusuf <naufal.pramudya@sg-dsa.com>
2024-03-01 05:13:03 +0700

    style: remove text-center from toast

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Toast = ({ success = false, failed = false, message = '' }) => {
   return (
-    <div className={`p-4 text-sm text-white fixed rounded-lg top-8 right-5 z-20 text-center
+    <div className={`p-4 text-sm text-white fixed rounded-lg top-8 right-5 z-20
             ${success ? 'bg-[#22c55e] w-[148px]'
             : failed ? 'bg-[#ef4444] w-80'
             : ''}`
